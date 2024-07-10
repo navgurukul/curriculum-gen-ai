c1db40886ea039f3164d9043b1a4f2a43c316cd6
Prateek Gole <152589865+prateekNG@users.noreply.github.com>
2024-07-10 19:42:11 +0530

    Bugfix
    Deleted a line by mistake.

--- Create-Scaffolded-Projects/main.js
+++ Create-Scaffolded-Projects/main.js
@@ -7,6 +7,7 @@ const { reviewAndModifyProjectGuide } = require('./reviewAndModifyProjectGuide')
 
 // get the key from the environment variable GEMINI_API_KEY and create a new instance of the GoogleGenerativeAI
 const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
 
 async function main() {
     // Generate project ideas
